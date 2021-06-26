cc3a0d3c8eadef71cb09febf695680ee25b421a4
paicrush <dcdc07411@gmail.com>
2021-06-26 13:30:16 +0700

    get

--- Services/PizzaService.cs
+++ Services/PizzaService.cs
@@ -20,29 +20,6 @@ namespace ContosoPizza.Services
         public static List<Pizza> GetAll() => Pizzas;
 
         public static Pizza Get(int id) => Pizzas.FirstOrDefault(p => p.Id == id);
-
-        public static void Add(Pizza pizza)
-        {
-            pizza.Id = nextId++;
-            Pizzas.Add(pizza);
-        }
-
-        public static void Delete(int id)
-        {
-            var pizza = Get(id);
-            if(pizza is null)
-                return;
-
-            Pizzas.Remove(pizza);
-        }
-
-        public static void Update(Pizza pizza)
-        {
-            var index = Pizzas.FindIndex(p => p.Id == pizza.Id);
-            if(index == -1)
-                return;
-
-            Pizzas[index] = pizza;
-        }
+       
     }
 }
